f2fbf09ae9ec891867b7a870f65e64b8ff9df39a
Moxy <keanu.knighton@gmail.com>
2023-02-09 20:55:51 +0000

    Added MapBoxGL plugin for interactive map implementation

--- assets/js/MapBox.js
+++ assets/js/MapBox.js
@@ -1,21 +1,13 @@
-
-
 let accessToken = 'pk.eyJ1IjoibW94eS0iLCJhIjoiY2xkdzIxem45MDJnbTNybnZ6dndpaHdycSJ9.myBLbSV4M9QmJmkYflKQ0Q';
-let queryURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/London.json?' + 'access_token=' + accessToken;
+let query = 'https://api.mapbox.com/geocoding/v5/mapbox.places/London.json?' + 'access_token=' + accessToken;
 
  $.ajax({
-        url: queryURL,
+        url: query,
         method: 'GET'
     })
-    .then(function(response) {
-        console.log(queryURL);
-        console.log(response);
+    .then(function(search) {
+        console.log(search);
+
 })
-.catch(function(error) {
-    console.log(error);
-});
-
-
-
 
 // https://api.mapbox.com/geocoding/v5/mapbox.places/London.json?access_token=pk.eyJ1IjoibW94eS0iLCJhIjoiY2xkdzIxem45MDJnbTNybnZ6dndpaHdycSJ9.myBLbSV4M9QmJmkYflKQ0Q
