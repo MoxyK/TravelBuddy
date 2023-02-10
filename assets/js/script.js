7606ad658fe42db63c810d9399026a1702d565d8
kalpana <kalpanarajasekar27@gmail.com>
2023-02-10 17:50:02 +0000

    upadted search input and submit button in html file

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  
+  appkey = "pub_1685584ea786012cacd69ff07a39293e82d98";
+
+//search function to get the news based on country
+  $("#btnSearch").click(function() {
+    let countrycode = $("#search-input").val().trim();
+    console.log(countrycode);
+    var qryLocationURL = "https://newsdata.io/api/1/news?apikey="+appkey+"&country="+countrycode;
+    console.log(qryLocationURL);
+
+    fetch (qryLocationURL)
+    .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+
+  }
+  
+  );
+
+    
+  //event.preventDefault();
+    
+  });
+
+
+});
