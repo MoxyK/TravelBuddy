370ae924edefbc24043488681d03a55f1356b555
Moxy <keanu.knighton@gmail.com>
2023-02-10 15:53:36 +0000

    search input returns Longitude/Latitude data - map.flyto not connecting yet

--- assets/js/MapBox.js
+++ assets/js/MapBox.js
@@ -1,13 +1,46 @@
 let accessToken = 'pk.eyJ1IjoibW94eS0iLCJhIjoiY2xkdzIxem45MDJnbTNybnZ6dndpaHdycSJ9.myBLbSV4M9QmJmkYflKQ0Q';
-let query = 'https://api.mapbox.com/geocoding/v5/mapbox.places/London.json?' + 'access_token=' + accessToken;
 
- $.ajax({
-        url: query,
-        method: 'GET'
+$(document).ready(function() {
+    $("#submit").click(function() {
+        let location = $("#searchInput").val();
+        let queryURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + location + '.json?access_token=' + accessToken;
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .then(function(search) {
+                // let result = outputData(response);
+                // $("#outputData").html(result);
+                // $("#outputData").val('');
+                    document.getElementById('submit').addEventListener('click', () => {
+                        // Fly to location
+                        map.flyTo({
+                        center: [(search.features[0].center)],
+                        essential: true // this animation is considered essential with respect to prefers-reduced-motion
+                        });
+                    });
+                    console.log(search.features[0].center);
+            })
+        
     })
-    .then(function(search) {
-        console.log(search);
-
 })
 
-// https://api.mapbox.com/geocoding/v5/mapbox.places/London.json?access_token=pk.eyJ1IjoibW94eS0iLCJhIjoiY2xkdzIxem45MDJnbTNybnZ6dndpaHdycSJ9.myBLbSV4M9QmJmkYflKQ0Q
+mapboxgl.accessToken = 
+'pk.eyJ1IjoibW94eS0iLCJhIjoiY2xkdzIxem45MDJnbTNybnZ6dndpaHdycSJ9.myBLbSV4M9QmJmkYflKQ0Q';
+let map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
+center: [-74.5, 40], // starting position [lng, lat]
+zoom: 9, // starting zoom
+});
+console.log(mapboxgl);
+
+
+// document.getElementById('submit').addEventListener('click', () => {
+//     // Fly to a random location
+//     map.flyTo({
+//     center: [(Math.random() - 0.5) * 360, (Math.random() - 0.5) * 100],
+//     essential: true // this animation is considered essential with respect to prefers-reduced-motion
+//     });
+//     });
