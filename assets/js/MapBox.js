bced241277ed5da7c0990496d92289bcef66101f
Moxy <keanu.knighton@gmail.com>
2023-02-12 14:51:02 +0000

    searchInput now fully functional with map - Location flyto. Full name display. ISO code display. Text clear on search.

--- assets/js/MapBox.js
+++ assets/js/MapBox.js
@@ -5,36 +5,54 @@ $(document).ready(function() {
         let location = $("#searchInput").val();
         let queryURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + location + '.json?access_token=' + accessToken;
 
+        // clear output section on subsequent searches
+        $("#outputData").empty();
+
             $.ajax({
                 url: queryURL,
                 method: "GET"
             })
+            // Return and consoleLog Longitude and Latitude as 2 seperate variables defined by searchInput.
             .then(function(searchLo) {
                 let long = (searchLo.features[0].center[0]);
                 let lati = (searchLo.features[0].center[1]);
                         console.log(long);
                         console.log(lati);
                         console.log(searchLo.features[0]);
-                            // Fly to a random location
+                            // Fly to location defined by longitude latitude variables
                             map.flyTo({
                             center: [(long), (lati)],
                             essential: true // this animation is considered essential with respect to prefers-reduced-motion
-                            })
+                            })  
+                })
 
-            .then(function(searchLo) {
-                let result = outputData(searchLo.context);
-                $("#outputData").html(result);
-                $("#outputData").val('');
-                
-                }) 
+                       // Next data call-function should display full location name and Country ISO tag in size scaling 
+                    // i.e -> [City: London, - Region: Greater London, - Country: England, United Kingdom: EN,GB] 
+            $.ajax({
+                url: queryURL,
+                method: "GET"
             })
+            
+            .then(function(searchName) {
+                let fullName = (searchName.features[0].place_name);
+                let ISOcode = (searchName.features[0].properties.short_code);
+                // MapBox data stores ISO in 2 possible locations -> if statement functions to find alternative data source
+                if(ISOcode === undefined) {
+                    ISOcodeAlt = (searchName.features[0].context[1].short_code);
+                    $("#outputData").append("<p><h5>" + fullName + ": " + ISOcodeAlt + "</h5></p>");
+                    $("#outputData").val('');
+                } else {
+                    $("#outputData").append("<p><h5>" + fullName + ": " + ISOcode + "</h5></p>");
+                    $("#outputData").val('');
+                }
 
-        function outputData(searchLo) {
-    return '<div><h4>' + searchLo.context[1].text
-        }
+                console.log(fullName);
+                console.log(ISOcode, ISOcodeAlt);
 
+            })
     })
 });
+
 
 mapboxgl.accessToken = 
 'pk.eyJ1IjoibW94eS0iLCJhIjoiY2xkdzIxem45MDJnbTNybnZ6dndpaHdycSJ9.myBLbSV4M9QmJmkYflKQ0Q';
