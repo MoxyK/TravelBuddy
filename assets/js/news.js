99442b07cebf26c4dd3713eda5e5b5cb4729c89e
kalpana <kalpanarajasekar27@gmail.com>
2023-02-15 15:45:33 +0000

    get the short code from map using localstorage and calling the function to display the news

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,26 +1,27 @@
-$(document).ready(function () {
-  
+//$(document).ready(function () {
+
   appkey = "pub_1685584ea786012cacd69ff07a39293e82d98";
   $("#news-header").hide();
 
-//search function to get the news based on country
-  $("#btnSearch").click(function() {
-    let countrycode = $("#search-input").val().trim();
-    console.log(countrycode);
+  function getnews(){
+     let countrycode= localStorage.getItem("countrycode");
+     console.log(countrycode);
+     //const ccarray = countrycode.split("-");
+     //let countryshortcode = ccarray[0].toLowerCase();
+    
     var qryLocationURL = "https://newsdata.io/api/1/news?apikey="+appkey+"&country="+countrycode;
     console.log(qryLocationURL);
 
     fetch (qryLocationURL)
     .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
+    .then((data) => {
+      console.log(data);
      
-    
       if(data.status=="success")
       {    
         $("#news-header").show();    
 
-        for(i=0; i<data.results.length; i++){
+        for(i=0; i<5; i++){
           console.log(data.results[i].title);
 
           htmlStr= '<div class="card">';
@@ -28,15 +29,12 @@ $(document).ready(function () {
           htmlStr+= '<h5 class="card-title"></h5>';
           htmlStr +='<p class="card-text"><a href="';
           htmlStr += data.results[i].link;
-          htmlStr+='">';
+          htmlStr+=' target=_blank">';
           htmlStr += data.results[i].title;
           htmlStr += '</a></p>';
           htmlStr+=' </div></div>';
 
-          
-
           $('#news-text').append(htmlStr);
-
     
       }
       
@@ -47,11 +45,6 @@ $(document).ready(function () {
   }
   
   );
+  }
 
-    
-  //event.preventDefault();
-    
-  });
-
-
-});
+//});
